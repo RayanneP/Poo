dc2d051dd092cb1f39462fb42f46fe21ff390517
RayanneP <rayannepicinidasilva@gmail.com>
2025-02-25 08:28:30 -0300

    Initial commit

---
+++ objeto.js
@@ -0,0 +1,18 @@
+//POO:
+//1- Classe:
+//Modelo para criar vários objetos
+//2-Objeto(atributo: propriedade, ação: método):
+//Representação de algo do mundo real
+//3-Pilares:
+//Abstração, encapsulamento, herança e polimorfismo.
+
+//criando um objeto de aluno
+const aluno = {
+    nome: "Raul",
+    idade:23,
+
+    estudar: () => console.log
+    (`${aluno.nome} está estudando.`),
+};
+console.log(aluno.nome);
+aluno.estudar();
